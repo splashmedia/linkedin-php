ee93f3449f5de60d1a53f54a5e083fad51afd6be
Daniel Cousineau <dcousineau@gmail.com>
2013-05-08 13:55:14 -0500

    Fixed mis-namespaced class

--- lib/oauth.php
+++ lib/oauth.php
@@ -750,7 +750,7 @@ class SimpleOAuthDataStore extends OAuthDataStore {/*{{{*/
 class OAuthUtil {/*{{{*/
   public static function urlencode_rfc3986($input) {/*{{{*/
   if (is_array($input)) {
-    return array_map(array('\LinkedIn\OAuthUtil','urlencode_rfc3986'), $input);
+    return array_map(array('\Splash\LinkedIn\OAuthUtil','urlencode_rfc3986'), $input);
   } else if (is_scalar($input)) {
     return str_replace('+', ' ',
                          str_replace('%7E', '~', rawurlencode($input)));
